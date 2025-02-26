cfcb0258831f9be0a7d1cebf2cd2a745521df023
erleeiah09 <jaloconerleeiah_bscs@plmun.edu.ph>
2025-02-26 13:26:30 +0800

    Last Sync: 2025-02-26 05:26:30 (Mobile)

--- newfile.java
+++ newfile.java
@@ -1,17 +1,12 @@
 public class MethodDemo {
-    // Method to print a greeting
-    public static void greet() {
-        System.out.println("Hello from the greet method!");
-    }
-
-    // Method to print a message
-    public static void printMessage(String message) {
-        System.out.println(message);
-    }
-
-    // Main method
     public static void main(String[] args) {
         greet(); // Calling the greet method
-        printMessage("This is my message!"); // Calling the printMessage method
+        printMessage("This is my message!");
+     }
+    public static void greet() {
+        System.out.println("Hello from the greet method!"); 
+     }
+    public static void printMessage(String message) {
+        System.out.println(message);
+     }
     }
-}
